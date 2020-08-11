c9565b225b0c847a63235f998e9ee14bb08fc3c0
sidsb <siddharthasb121@gmail.com>
2020-08-11 20:37:03 +0530

    bug fix

---
+++ build/precache-manifest.1f3c846f3f81d825c25a38146c6d7701.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087609d4c64a3eb312c61ded65bec86a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675fb4bab3fce25569b8",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bc45f953e27c43daa546",
+    "url": "/static/css/main.28c2909e.chunk.css"
+  },
+  {
+    "revision": "675fb4bab3fce25569b8",
+    "url": "/static/js/2.0e359565.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0e359565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc45f953e27c43daa546",
+    "url": "/static/js/main.98159676.chunk.js"
+  },
+  {
+    "revision": "dc20b4a4b9c4a9ca3c78",
+    "url": "/static/js/runtime-main.a73b6cbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
